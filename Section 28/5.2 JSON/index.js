fe7cb789b1ff40a367e88df3c3860d9562348bcc
MD Reduanul Hoque <mdreduanulhoquesadik@gmail.com>
2026-02-16 20:51:42 +0600

    Completed Section 28/ 5.2 task

--- Section 28/5.2 JSON/index.js
+++ Section 28/5.2 JSON/index.js
@@ -13,12 +13,24 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {recipe : data});
 });
+
+let data;
 
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+
+  if(req.body.choice == "chicken"){
+    data = JSON.parse(recipeJSON)[0];
+  }else if(req.body.choice == "beef"){
+    data = JSON.parse(recipeJSON)[1];
+  }else if(req.body.choice == "fish"){
+    data = JSON.parse(recipeJSON)[2];
+  }
+  
+  res.render("index.ejs", {recipe : data});
 });
 
 app.listen(port, () => {
